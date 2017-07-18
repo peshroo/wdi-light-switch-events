bd9c8f36248e4aa07bce9ab5fc571e085edeba56
Roopesh <rrattan.89@gmail.com>
2017-07-18 16:16:39 -0400

    switch btn classes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(event){
   var lighSwitch = document.querySelector('button');
+  var body       = document.querySelector('body');
+  var status     = document.querySelector('.status');
 
   lightSwitch.addEventListener('click', function(event){
-    lightSwitch.className = 'Switch Off';
+    if (lightSwitch.className === 'switch on') {
+      lightSwitch.className = 'switch off';
+      body.className = 'dark';
+      status.innerText = 'Hey! Who turned off the lights?';
+    } else {
+      lightSwitch.className = 'switch on'
+      body.className = 'light';
+      status.innerText = "It's so bright in here!"
+    }
 
-    var body = document.querySelector('body');
-    body.className = 'dark';
-
-    var status = document.querySelector('.status');
-    status.innerText = 'Hey, who turned off the lights?!?';
-  })
 });
