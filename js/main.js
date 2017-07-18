37664a04c8a6e7d810a4e2819be7714ad67801aa
Roopesh <rrattan.89@gmail.com>
2017-07-18 16:12:48 -0400

    click listener to switch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(event){
-  var status = document.querySelector('.status');
-  status.innerText = "The document is ready!";
+  var lighSwitch = document.querySelector('button');
+
+  lightSwitch.addEventListener('click', function(event){
+    lightSwitch.className = 'Switch Off';
+
+    var body = document.querySelector('body');
+    body.className = 'dark';
+
+    var status = document.querySelector('.status');
+    status.innerText = 'Hey, who turned off the lights?!?';
+  })
 });
